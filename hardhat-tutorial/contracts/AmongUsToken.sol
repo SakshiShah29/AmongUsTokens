48d14a7411772069f923a7db6c60880b98f91130
SakshiShah29 <sakshi.shah299@gmail.com>
2022-09-13 19:40:06 +0530

    Contract Deployed🚀

--- hardhat-tutorial/contracts/AmongUsToken.sol
+++ hardhat-tutorial/contracts/AmongUsToken.sol
@@ -21,8 +21,76 @@ pragma solidity ^0.8.0;
       IAmongUs AmongUsNFT;
 
       //mapping to keep the track of which tokenIds have been claimed
-      mapping(uint256=>bool) public tokenIdsClaimmed;
+      mapping(uint256=>bool) public tokenIdsClaimed;
 
-      contrutor()
+      constructor(address _amoungUsContract) ERC20("Crypto Dev Token", "CD") {
+          AmongUsNFT = IAmongUs(_amoungUsContract);
+      }
+
+
+      //Mints `amount` number of AmongUsTokens
+      //msg.value  should be equal or greater than amount*tokenprice
+      function mint(uint256 amount) public payable{
+        // the value of ether that should be equal or greater than tokenPrice * amount;
+        uint256 _requiredAmount=tokenPrice*amount;
+        require(msg.value>_requiredAmount,"Ether sent is incorrect!");
+        //total tokens +amount <=10000,otherwise revert the transaction
+        uint256 amountWithDecimals=amount*10**18;
+        require(
+          (totalSupply()+amountWithDecimals)<=maxTotalSupply,"Exceeds the max total supply available"
+        );
+        //call the internal function from openzepplins ERC20 contract
+        _mint(msg.sender,amountWithDecimals);
+      }
+        /**
+       * @dev Mints tokens based on the number of NFT's held by the sender
+       * Requirements:
+       * balance of Crypto Dev NFT's owned by the sender should be greater than 0
+       * Tokens should have not been claimed for all the NFTs owned by the sender
+       */
+
+      function claim() public{
+        address sender=msg.sender;
+        //get the number of amoung us nfts held by the sender address
+        uint256 balance=AmongUsNFT.balanceOf(sender);
+        //If the balance is zero revert the transaction
+        require(balance>0,"You dont own any Among Us NFTS");
+        //amount keeps track of the number of unclaimed tokenids
+        uint256 amount=0;
+        //loop over the balance and get the token ID owned by "sender" at a given "index" of its token list
+        for(uint256 i=0;i<balance;i++){
+          uint256 tokenId=AmongUsNFT.tokenOfOwnerByIndex(sender,i);
+          //if the tokenId has not been claimed,increase the amount
+          if(!tokenIdsClaimed[tokenId]){
+            amount+=1;
+            tokenIdsClaimed[tokenId]=true;
+          }
+        }
+        //if all the token ids have been claimed , revert the transaction
+        require(amount>0,"You have already claimed all the tokens");
+        //call the internal function from openzeppelins ERC20 contract
+        //Mint (amount*10) tokens per nft
+        _mint(msg.sender,amount*tokensPerNFT); 
+      }
+
+       /**
+        * @dev withdraws all ETH and tokens sent to the contract
+        * Requirements: 
+        * wallet connected must be owner's address
+        */
+       function withdraw() public onlyOwner{
+        address _owner=owner();
+        uint256 amount = address(this).balance;
+        (bool sent, )=_owner.call{value:amount}("");
+        require(sent,"Failed to send Ether");
+       }
+
+      // Function to receive Ether. msg.data must be empty
+      receive() external payable {}
+
+      // Fallback function is called when msg.data is not empty
+      fallback() external payable {}
+
+       
 
   }
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,31 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers}=require("hardhat");
+require("dotenv").config({path:".env"});
+const {AMONG_US_NFT_CONTRACT_ADDRESS}=require("../constants");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+async function main(){
+  // address of the the among us  contract
+  const amongUsNFTContract=AMONG_US_NFT_CONTRACT_ADDRESS;
+  
+  /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
+    */
+   const amongUsTokenContract=await ethers.getContractFactory("AmongUsToken");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+   //deploy the contract
+   const deployedAmongUsTokenContract=await amongUsTokenContract.deploy(amongUsNFTContract);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+   //print the address of the deployed contract
+   console.log(
+    "Among Us Token Contract Address:",
+    deployedAmongUsTokenContract.address
+   )
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
